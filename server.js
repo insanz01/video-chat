6380281f1001c715bdb0395abb63290b8d1ca144
insanz01 <insankamil002@gmail.com>
2023-01-05 13:31:33 +0700

    update syntax

--- server.js
+++ server.js
@@ -24,6 +24,19 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+app.get("/chat", (req, res) => {
+  console.log("ada yang join nih");
+  io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+      socket.join(roomId);
+      socket.to(roomId).broadcast.emit("user-connected", userId);
+      socket.on("message", (message) => {
+        io.to(roomId).emit("createMessage", message, userName);
+      });
+    });
+  });
+});
+
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
